c9eacd97605cc528bc614af329a42b9183cbf3ab
Tyler Sheaves <sheaves94@gmail.com>
2023-08-16 21:32:17 -0700

    Fixed SDC issue

--- EXPERIMENTS/quartus_projects/niosv_aes/constraints/clock_assignments.tcl
+++ EXPERIMENTS/quartus_projects/niosv_aes/constraints/clock_assignments.tcl
@@ -1,2 +1,3 @@
 set_instance_assignment -name GLOBAL_SIGNAL GLOBAL_CLOCK -to "soc_system:u0|soc_system_theta_clks:theta_clks|altera_pll:altera_pll_i|altera_cyclonev_pll:cyclonev_pll|divclk[0]"
 set_instance_assignment -name GLOBAL_SIGNAL GLOBAL_CLOCK -to "soc_system:u0|soc_system_theta_clks:theta_clks|altera_pll:altera_pll_i|altera_cyclonev_pll:cyclonev_pll|divclk[1]"
+set_instance_assignment -name MATCH_PLL_COMPENSATION_CLOCK ON -to "soc_system:u0|soc_system_theta_clks:theta_clks|altera_pll:altera_pll_i|altera_cyclonev_pll:cyclonev_pll|divclk[0]"
