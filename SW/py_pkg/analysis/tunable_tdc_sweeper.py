9cb2d273cb439566a9ddedb782d9e6d522f81115
Tyler Sheaves <sheaves94@gmail.com>
2023-08-17 18:27:22 -0700

    Updates

--- SW/py_pkg/analysis/tunable_tdc_sweeper.py
+++ SW/py_pkg/analysis/tunable_tdc_sweeper.py
@@ -8,6 +8,7 @@ import matplotlib.pyplot as plt
 plt.rcParams['figure.dpi'] = 300
 from matplotlib.ticker import MultipleLocator
 from fractions import Fraction
+import warnings
 
 class TunableTDCSweeper():
     def __init__(
@@ -424,19 +425,46 @@ class TunableTDCSweeper():
                 self.tuned_theta_minvar_fall = min_var
                 tuned_val = self.tuned_theta_minvar_fall
         
-        print(f"Theta tuning complete! {tuning_param}={tuned_val}")
+        edge = "rising" if tune_rise else "falling"
+        print(f"Theta tuning complete! {tuning_param} {edge}={tuned_val}")
         print(f"\t          M:{self.tuned_theta_m}")
         print(f"\t          N:{self.tuned_theta_n}")
         print(f"\t          C:{self.tuned_theta_c}")
         print(f"\tPhase Bumps:{self.tuned_ps_bumps_theta}")
         print(f"\t Delay (ps):{self.tuned_theta_delay_ps}")
 
-    def sweep_phi(self, phi_cfg_params, theta_samples, sweep_data_fh="phi_sweep.csv"):
-
+    def sweep_phi(self, phi_cfg_params, theta_samples, mode="bg", perpetual=False):
+        
         tdc_sample_bytes     = ceil(self.tdc_len/8)
         theta_samples_bytes  = theta_samples * tdc_sample_bytes
         max_transfer_samples = floor(self.dma.max_transfer/tdc_sample_bytes)
-
+        
+        if(mode == "bg"):
+            phi_sweep_plot_data = {
+                "delta mu fall bg"     : [],
+                "delta mu rise bg"     : [],
+                "var fall bg"          : [],
+                "var rise bg"          : [],
+                "phi delay (ps) bg"    : []
+            }
+            # Hold IP in reset during BG measurement
+            self.target_ip.hold_reset()
+        elif(mode == "target"):
+            phi_sweep_plot_data = {
+                "delta mu fall target" : [],
+                "delta mu rise target" : [],
+                "var fall target"      : [],
+                "var rise target"      : [],
+                "phi delay (ps) target": []
+            }
+            # Reset IP and start
+            self.target_ip.perpetual=perpetual
+            ip_sync_en = 0 if perpetual==True else 1
+            self.target_ip.reset()
+        else:
+            warnings.warn(f"Unsupported mode {mode}. Exiting!")
+            return -1
+        
         with progressbar.ProgressBar(max_value=len(phi_cfg_params.index)) as bar:
             # Sweep through theta PLL configurations to generate a calibrated theta offset
             for index, row in phi_cfg_params.iterrows():
@@ -449,8 +477,6 @@ class TunableTDCSweeper():
                 ps_bumps = int(row['n'])
                 delay_ps = row['delay (ps)']
                 
-                # Hold IP in reset during PLL reconfiguration
-                self.target_ip.hold_reset()
                 # Hold TDC in reset during PLL reconfiguration
                 self.pulsegen.hold_reset()
                 # Hold theta PLL in reset during phi PLL reconfiguration
@@ -472,8 +498,7 @@ class TunableTDCSweeper():
                 # Complete reset sequence of TDC
                 self.pulsegen.reset()
                 
-                if(mode == "bg"):
-                
+                if(mode == "bg"):                    
                     # Collect samples
                     samples = []
                     theta_samples_temp = theta_samples
@@ -503,13 +528,13 @@ class TunableTDCSweeper():
                     mean_falling_pop_bg, mean_rising_pop_bg, var_pop_falling_bg, var_pop_rising_bg \
                         = self.__process_pop_counts__(pop_counts_rising, pop_counts_falling )
                     
-                    phi_sweep_plot_data["phi delay (ps)"].append(delay_ps)
+                    phi_sweep_plot_data["phi delay (ps) bg"].append(delay_ps)
                     phi_sweep_plot_data["delta mu fall bg"].append(mean_falling_pop_bg - self.tuned_theta_avg_fall)
                     phi_sweep_plot_data["delta mu rise bg"].append(mean_rising_pop_bg - self.tuned_theta_avg_rise)
                     phi_sweep_plot_data["var fall bg"].append(var_pop_falling_bg)
                     phi_sweep_plot_data["var rise bg"].append(var_pop_rising_bg)
 
-                if(mode=="target"):
+                elif(mode=="target"):
                     
                     # Collect samples, and calculate averages
                     samples = []
@@ -517,12 +542,10 @@ class TunableTDCSweeper():
 
                     # Sample sequences
                     while(theta_samples_temp > max_transfer_samples):
-                        # Reset target IP
-                        self.target_ip.reset()
                         # Prepare target IP
                         self.target_ip.pre()
                         # Prime pulse generator
-                        self.pulsegen.prime(1, theta_samples_temp)
+                        self.pulsegen.prime(ip_sync_en, theta_samples_temp)
                         # Run target IP
                         self.target_ip.run()
                         # Collect samples
@@ -531,16 +554,14 @@ class TunableTDCSweeper():
                         # Wait for target IP to wrap
                         self.target_ip.post()
 
-                    # Reset target IP
-                    self.target_ip.reset()
                     # Prepare target IP
                     self.target_ip.pre()
                     # Prime pulse generator
-                    self.pulsegen.prime(1, theta_samples_temp)
+                    self.pulsegen.prime(ip_sync_en, theta_samples_temp)
                     # Run target IP
                     self.target_ip.run()
                     # Collect samples
-                    samples = samples + self.dma.get_dma_words(max_transfer_samples, tdc_sample_bytes)
+                    samples = samples + self.dma.get_dma_words(theta_samples_temp, tdc_sample_bytes)
                     # Wait for target IP to wrap
                     self.target_ip.post()
 
@@ -555,14 +576,16 @@ class TunableTDCSweeper():
                     # Calculate mean and variance of background pop counts
                     mean_falling_pop_target, mean_rising_pop_target, var_pop_falling_target, var_pop_rising_target \
                         = self.__process_pop_counts__(pop_counts_rising, pop_counts_falling )
-
+                    
+                    phi_sweep_plot_data["phi delay (ps) target"].append(delay_ps)
                     phi_sweep_plot_data["delta mu fall target"].append(mean_falling_pop_target - self.tuned_theta_avg_fall)
                     phi_sweep_plot_data["delta mu rise target"].append(mean_rising_pop_target - self.tuned_theta_avg_rise)
                     phi_sweep_plot_data["var fall target"].append(var_pop_falling_target)
                     phi_sweep_plot_data["var rise target"].append(var_pop_rising_target)
 
+        return phi_sweep_plot_data
                     
-    def save_bg_target_data(self, bg={}, target{}, sweep_data_fh="./phi_sweep_data.csv"):
+    def save_bg_target_data(self, bg={}, target={}, sweep_data_fh="./phi_sweep_data.csv"):
         # Check output dir
         output_dir = os.path.dirname(os.path.realpath(sweep_data_fh))
         isExist = os.path.exists(output_dir)
@@ -572,7 +595,7 @@ class TunableTDCSweeper():
         
         # Merge bg and target sweeps
         total = bg
-        total = total.merge(target)
+        total.update(target)
         total['diff mu fall']   = []
         total['diff mu rise']   = []
         total['diff var fall']  = []
@@ -586,7 +609,7 @@ class TunableTDCSweeper():
                 total["var fall target"], total["var fall bg"],
                 total["var rise target"], total["var rise bg"]):
             
-            total['diff mu fall'].append(mu_fpop_targ - mu_fpop_bg)
+            total['diff mu fall'].append(mu_fpop_tg - mu_fpop_bg)
             total['diff mu rise'].append(mu_rpop_tg - mu_rpop_bg)
             total['diff var fall'].append(vr_fpop_tg - vr_fpop_bg)
             total['diff var rise'].append(vr_rpop_tg - vr_rpop_bg)
