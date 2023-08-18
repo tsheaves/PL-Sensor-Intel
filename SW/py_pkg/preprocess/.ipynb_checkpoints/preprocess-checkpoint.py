ffd6dfc5d042e31d2b60dc538313fbbc6069838e
Tyler Sheaves <sheaves94@gmail.com>
2023-08-18 11:16:07 -0700

    Preprocess now jitter/phase noise aware

--- SW/py_pkg/preprocess/.ipynb_checkpoints/preprocess-checkpoint.py
+++ SW/py_pkg/preprocess/.ipynb_checkpoints/preprocess-checkpoint.py
@@ -6,8 +6,8 @@ class PLLPreProcessor():
 
     def __init__(
         self, vendor='intel', vco_max=1600*10**6, \
-        vco_min=600*10** 6, Ms=(1, 512), \
-        Ds=(1, 512), Os=(1,512) 
+        vco_min=600*10**6, Ms=range(1, 512), \
+        Ds=range(1, 512), Os=range(1,512) 
     ):
         self.vendor  = vendor.lower()
         self.vco_max = vco_max
@@ -15,13 +15,13 @@ class PLLPreProcessor():
         self.Ms = Ms
         self.Ds = Ds
         self.Os = Os
-        self.max_f_pfd = self.vco_max/max(M)
+        self.max_f_pfd = self.vco_max/max(Ms)
         self.max_o = max(Os)
     
-    def is_between(a, x, b):
+    def is_between(self, a, x, b):
         return min(a, b) <= x <= max(a, b)
 
-    def __stability_metric__(self, f_pfd, f_vco, O):
+    def __calc_stability_metric__(self, f_pfd, f_vco, O):
         '''
         TODO check this
             Weigh the following equally:
@@ -34,16 +34,16 @@ class PLLPreProcessor():
         return prop_max_pfd + prop_max_vco + prop_max_o
     
     def gen_pll_params( self, \
-        ref_clk  = 50 * 10 ** 6,  \ 
+        ref_clk  = 50 * 10 ** 6, \
         out_min = 100 * 10 ** 6, out_max = 100 * 10 ** 6, \
-        delay_lower = 0 * 10 ** -12, \ 
+        delay_lower = 0 * 10 ** -12, \
         delay_upper = 6000 * 10 ** -12):
         
         # Fine Grain Divider setting
         fgdiv = 56 if self.vendor == 'xilinx' else 8
         
         # Take the cross product and filter to keep all M/D values that produce valid Fvco frequencies
-        vcos = [(M,D) for (M,D) in product(self.Ms, self.Ds) if is_between(vco_min, ref_clk * M / D, self.vco_max)]
+        vcos = [(M,D) for (M,D) in product(self.Ms, self.Ds) if self.is_between(self.vco_min, ref_clk * M / D, self.vco_max)]
         
         arr = []
         for (M, D) in vcos:
@@ -53,39 +53,41 @@ class PLLPreProcessor():
                 if( (out > out_max) or (out < out_min) ):
                     continue # Skip loop iteration
                 for n in range (0, round(fgdiv * ref_clk * M / (D * out))):
-                    delay = round(D * O / (ref_clk * M) - n / (fgdiv * out * O), 15)
-                    f_pfd = f_ref/M
+                    delay = D * O / (ref_clk * M) - n / (fgdiv * out * O)
+                    f_pfd = ref_clk/M
                     f_vco = ref_clk * M / D
-                    stability_metric = self.stability_metric(f_pfd, f_vco, O)
-                    if(is_between(delay_lower, delay, delay_upper)):
+                    stability_metric = self.__calc_stability_metric__(f_pfd, f_vco, O)
+                    if(self.is_between(delay_lower, delay, delay_upper)):
                         arr.append((M, D, O, n, f_vco, f_pfd, ref_clk, \
-                                    out, out / (10 ** 6), delay, delay / (10 ** -12) \
+                                    out, out / (10 ** 6), delay, delay / (10 ** -12), \
                                     stability_metric))
                         
         df = pd.DataFrame(arr, columns = ["M", "D", "O", "n", "Fvco", "Fpfd", "Fref", \
-                                          "Fout", "Fout (MHz)", "delay", "delay (ps)" \
-                                          "stability_metric"])
+                                          "Fout", "Fout (MHz)", "delay", "delay (ps)", \
+                                          "stability metric"])
         
-        df["delay (ps)"] = list(np.around(np.array(df["delay (ps)"]),32))
-        df = df.sort_values(["delay", "Fvco"]).drop_duplicates(subset = "delay", keep = "first")
+        df = df.sort_values(["delay", "Fvco"])
         
         return df
 
     def optimize_pll_configs(self, df, delay_min_ps, delay_max_ps, \
                              res_ps=10, metric="stability"):
-        step = res_ps/2
+        step = int(res_ps/2)
         opt_df = pd.DataFrame(columns=df.columns.values.tolist())
         if(metric == "stability"):
-            for delay in range (delay_min, delay_max-res_ps, res_ps):
+            for delay in range (int(delay_min_ps), int(delay_max_ps-res_ps), res_ps):
                 window_max = delay + res_ps
                 window_min = delay
-                # Get all candidate configurations
-                df_tmp = df[[df["delay (ps)"] > window_min]]
+                if not df.empty:
+                    # Get all candidate configurations
+                    df_tmp = df[df["delay (ps)"] >= window_min]
+                else:
+                    continue
                 if not df_tmp.empty:
-                    df_tmp = df_tmp[[df_tmp["delay (ps)"] <= window_max]]
+                    df_tmp = df_tmp[df_tmp["delay (ps)"] < window_max]
                 if not df_tmp.empty:
                     # Filter configurations with the highest stability metric
-                    df_tmp = df_tmp[[df_tmp["stability metric"] == df_tmp["stability metric"].max()]]
+                    df_tmp = df_tmp[df_tmp["stability metric"] == df_tmp["stability metric"].max()]
                     opt_df = pd.concat([opt_df, df_tmp], axis=0)
         
         return opt_df
